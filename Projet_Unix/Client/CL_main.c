d353de6899332d130d18084d64b0e66cc750c917
clairorx <84668330+clairorx@users.noreply.github.com>
2023-03-22 10:26:15 +0100

    Création messagerie

--- Projet_Unix/Client/CL_main.c
+++ Projet_Unix/Client/CL_main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+/**********************************************
+ * Projet Unix - Client
+ * 	
+ * Taches 
+*/
+#include "CL_include"
+
+static void end(); 
+static int Msqid;
 
 int main(){
 	printf("Test Client\n");
+	Msqid = CreationMessagerie();
+	printf("msqid = %d\n", Msqid);
 	return 0;
+}
+
+static void end() { 
+  printf("\nSrv:FIN Mort du fils receptionniste \n");
+  printf("Srv:FIN Mort du fils informateur \n");
+  printf("\nSrv:FIN RelachMsg %d\n", RelacheMessagerie(Msqid));
+  kill(0,SIGKILL); /* on tue tout le monde */
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Projet_Unix
 Projet Unix Serveur - Client 
+
+# A Savoir pour mac : 
+dans SV_include -> Changer sys/signal.h par signal.h
+Pour compiler : make temps de création du serveur en secondes 
+
+SV.c compilé, on obtient un fichier binaire sv_zz 
