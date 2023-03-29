03c0051139d421febc2b170158cc74883dda0942
clairorx <84668330+clairorx@users.noreply.github.com>
2023-03-29 10:06:32 +0200

    Membuf et voie

--- Projet_Unix/Client/CL_data.h
+++ Projet_Unix/Client/CL_data.h
@@ -7,7 +7,7 @@
 /**
  * @brief Fonction qui permet de lire les données dans la mémoire partagée
  * 
- * @param shmid 
+ * @param voie
  * @param MemBuf 
  */
-void read_data(int shmid, BUF **MemBuf);
+void read_data(int voie, BUF **MemBuf);
