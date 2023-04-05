8f7145c42be11f67bbdd4a0068c3853053c08c35
Tristan <titou.lailler@gmail.com>
2023-04-05 10:37:30 +0200

    ajout exit(0) dans redacteurs

--- Projet_Unix/Client/CL_main.c
+++ Projet_Unix/Client/CL_main.c
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]){
 				exit(0);
 			}
 			else{ /* Code du Redacteur 1 */
-				
+				exit(0);
 			}
 		}
 	}
@@ -81,7 +81,7 @@ int main(int argc, char *argv[]){
 				exit(0);
 			}
 			else{ /* Code du Redacteur 0 */
-				
+				exit(0);
 			}
 	}
 	waitpid(pidLecteur0,NULL,0);
