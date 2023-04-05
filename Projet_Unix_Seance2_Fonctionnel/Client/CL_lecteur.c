914ba0c1c888d3db3c23acc5d62163a76fe5b390
clairorx <84668330+clairorx@users.noreply.github.com>
2023-04-05 08:33:11 +0200

    Clean + séance 2

--- Projet_Unix_Seance2_Fonctionnel/Client/CL_lecteur.c
+++ Projet_Unix_Seance2_Fonctionnel/Client/CL_lecteur.c
@@ -6,6 +6,14 @@
 
 #include "CL_include"
 
+/**
+ * @brief Fonction qui permet de lire les données dans la mémoire partagée
+ * 
+ * @param voie 
+ * @param sem 
+ * @param MemBuf 
+ * @param limiteN 
+ */
 void main_lecteur(int voie, int sem, BUF **MemBuf, int limiteN){
 	while(((*(MemBuf)+0)->n + (*(MemBuf)+1)->n)<limiteN){
 		P(sem,voie);
